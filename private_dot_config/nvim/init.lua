32e93ee7ba8da487db517e506bbfe1170a16801e
Masayuki Shamoto <shamoto_masayuki@nintendo.co.jp>
2026-04-03 09:36:19 +0900

    improve error display

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -189,6 +189,13 @@ vim.diagnostic.config {
 
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
+-- Auto open diagnostic float on CursorHold
+vim.api.nvim_create_autocmd('CursorHold', {
+  callback = function()
+    vim.diagnostic.open_float(nil, { focus = false })
+  end,
+})
+
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
