41a029921760d5e9652d1977f1103159ea422704
shamaton <shamaton77@gmail.com>
2026-04-22 10:17:15 +0900

    update zsh setting for large repositories

--- dot_p10k.zsh
+++ dot_p10k.zsh
@@ -30,15 +30,26 @@
   [[ $ZSH_VERSION == (5.<1->*|<6->.*) ]] || return
 
   # The list of segments shown on the left. Fill it with the most important segments.
-  typeset -g POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(
-    # =========================[ Line #1 ]=========================
-    os_icon                 # os identifier
-    dir                     # current directory
-    vcs                     # git status
-    # =========================[ Line #2 ]=========================
-    newline                 # \n
-    # prompt_char           # prompt symbol
-  )
+  if [[ "${P10K_DISABLE_VCS:-0}" == 1 ]]; then
+    typeset -g POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(
+      # =========================[ Line #1 ]=========================
+      os_icon                 # os identifier
+      dir                     # current directory
+      # =========================[ Line #2 ]=========================
+      newline                 # \n
+      prompt_char             # prompt symbol
+    )
+  else
+    typeset -g POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(
+      # =========================[ Line #1 ]=========================
+      os_icon                 # os identifier
+      dir                     # current directory
+      vcs                     # git status
+      # =========================[ Line #2 ]=========================
+      newline                 # \n
+      prompt_char             # prompt symbol
+    )
+  fi
 
   # The list of segments shown on the right. Fill it with less important segments.
   # Right prompt on the last prompt line (where you are typing your commands) gets
